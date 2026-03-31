311d3e36234ae72b4c04963fe8578b489d0673ae
ledatuet <ledatandu@gmail.com>
2026-03-31 19:16:33 +0700

    commitne

--- codepython.py
+++ codepython.py
@@ -1,3 +1,20 @@
-import random
-set = random.randint(1,3)
-print(f'So set can hoc la: {set}')
+def nhap(n):
+    a = str(n)
+    l = len(a)
+    for i in range(0,l//2):
+        if a[i] != a[l-1-i]:
+            return False
+        return True
+def nguoc(n):
+    k = 0
+    while n > 0:
+        k = k*10 + n%10
+        n //= 10
+    return k
+n = int(input())
+k = 0
+while n < 10000:
+    if nhap(n):
+        print(k,n)
+    n = n+nguoc(n)
+    k += 1
